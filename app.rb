45650c261c48410f7dd01a8058c07eef39822d91
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-21 18:48:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 class Application < Sinatra::Base
-  Sinatra::Base 
-  
-  get '/' do 
+  # Old route from previous tests
+  get '/' do
     erb :index
-  end 
-
+  end
+ 
+  # New route to respond to the form submission
+  post '/greet' do
+    erb :greet
+  end
 end
